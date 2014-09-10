21a72bf4beaa5351118cd7e4d384371d8135bd18
Yu-Fu Wu <wennynnewenny@gmail.com>
2014-09-10 23:48:24 +0000

    command interpreter

---
+++ src/autokey.py
@@ -0,0 +1,27 @@
+#!/bin/env python
+
+import sys
+import os
+
+def transform(string):
+    if "\"" in string:
+        return ['type'] + ['\"' + char + '\"' for char in string.replace('\"', '')]
+    else:
+        wdict = {'M-':'Alt+',
+                 'C-':'Ctrl+',
+                 'S-':'Shift+'}
+        for key in wdict:
+            string = string.replace(key, wdict[key])
+        return ['key'] + string.split(' ')
+
+with open(sys.argv[1], 'r') as command:
+    os.system('mkdir ' + sys.argv[2])
+    for index,line in enumerate(command):
+        key = transform(line.replace('\n', ''))
+        func = key[0]
+        operate = key[1:]
+        print(key)
+        for single in operate:
+            os.system("xdotool " + func + " " + single)
+        os.system('scrot ' + sys.argv[2] + '/' + 
+                  'screen_' + str(index) + '00.png')
